91202bb49a01a62f01167090423f9c22ced5a06c
Matias Silva <mw781@srcf.net>
2021-01-24 09:35:13 +0000

    Improving script w comments

--- refresh.sh
+++ refresh.sh
@@ -4,6 +4,7 @@ FILES_DIR="/societies/cupor/cupor-web"
 EXPORT_DIR="/public/societies/cupor/public_html"
 
 rm -rf "${EXPORT_DIR}"/*
+echo "Deleted all files in ${EXPORT_DIR}"
 cd $FILES_DIR
 git pull
 bundle exec jekyll build -d ../public_html
