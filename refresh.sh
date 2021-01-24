e559b122ae970640f4bf0c2902685cdf13053e67
Matias Silva <mw781@srcf.net>
2021-01-24 09:34:07 +0000

    Trigger bash globbing

--- refresh.sh
+++ refresh.sh
@@ -3,7 +3,7 @@
 FILES_DIR="/societies/cupor/cupor-web"
 EXPORT_DIR="/public/societies/cupor/public_html"
 
-rm -rf "${EXPORT_DIR}/*"
+rm -rf "${EXPORT_DIR}"/*
 cd $FILES_DIR
 git pull
 bundle exec jekyll build -d ../public_html
